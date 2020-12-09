612eb4c59f96521e84f7f5802d83aca870e9454c
LucasVeloz <lurucell@gmail.com>
2020-12-09 13:11:42 -0300

    tela de cadastro quase finalizada, faltando masterizar imagem

--- src/components/cadastro/index.js
+++ src/components/cadastro/index.js
@@ -11,22 +11,24 @@ const Cadastro = () => {
                 <img src={imagem}></img>
             </div>
             <div className='half screen'>
-                <img src={logo}></img>
-                <form>
-                    <p>Nome</p>
-                    <input></input>
-                    <p>Email</p>
-                    <input></input>
-                    <p>Senha</p>
-                    <input type='password'></input>
-                    <p>Confirme sua Senha</p>
-                    <input type='password'></input>
-                    <input type='submit' className='button-submit' value='CADASTRAR'></input>
-                </form>
-                <div className='login'>
-                    <p className='textLogin'>Já Possui conta? </p>
-                    <a href='/' className='Link'> Faça Login</a>
-                    {/* <Link to='/'> Faça Login</Link> */}
+                <div className='centralizer'>
+                    <img src={logo}></img>
+                    <form>
+                        <p>Nome</p>
+                        <input></input>
+                        <p>Email</p>
+                        <input></input>
+                        <p>Senha</p>
+                        <input type='password'></input>
+                        <p>Confirme sua Senha</p>
+                        <input type='password'></input>
+                        <input type='submit' className='button-submit' value='CADASTRAR'></input>
+                    </form>
+                    <div className='login'>
+                        <p className='textLogin'>Já possui conta? </p>
+                        <a href='/' className='Link'> Faça Login</a>
+                        {/* <Link to='/'> Faça Login</Link> */}
+                    </div>
                 </div>
             </div>
         </div>
