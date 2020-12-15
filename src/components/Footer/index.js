97761d43582ab092319a06b5c5b81622e3ad4399
VHvictor1 <victoroliveira@cjr.org.br>
2020-12-15 19:40:27 -0300

    Add Footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+
+import "./style.css";
+
+const Footer = () =>{
+  return (
+    <div className = 'Footer'>
+      <p>&#169; 2020 - Todos os direitos reservados. Desenvolvido por petgatô-grupo(2). </p>
+    </div>
+  )
+}
+export default Footer;
