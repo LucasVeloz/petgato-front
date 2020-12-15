e465841404f14232e2b14d5897d5dc4a3244a8e2
LucasVeloz <lurucell@gmail.com>
2020-12-15 20:04:25 -0300

    hotfix

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -3,31 +3,38 @@ import { useCallback, useState } from "react";
 import { Link } from 'react-router-dom';
 import Logo from '../../assets/gatinho_petgato.svg'
 import api from "../../services/api";
-
+import { useHistory } from 'react-router-dom';  
 const LoginPage = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const history = useHistory();
+    const handleSubmit = useCallback(async (e) => {
+        e.preventDefault();
+        try{
 
-    const handleSubmit = useCallback(async () => {
-        const { data } = await api.post("/auth/login", {
-            email,
-            password,
-        });
-        const token = data.token;
-        localStorage.setItem("@PetGato:token", token);
-    }, [email, password]);
+            const { data } = await api.post("/auth/login", {
+                email,
+                password,
+            });
+            const token = data.token;
+            localStorage.setItem("@PetGato:token", token);
+            history.replace('/');
+        }catch (e) {
+            alert("Houve um erro, tente novamente mais tarde :)");
 
+        }
+    }, [email, password, history]);
     return (
         <div className='container-max login'>
             <div className='half screen'>
                 <div className='centralizer'>
                     <img src={Logo} alt="PetGato"></img>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <p>Email</p>
                         <input type='email' autoComplete='email' onChange={(e) => setEmail(e.target.value)}></input>
                         <p>Senha</p>
                         <input type='password' onChange={e => setPassword(e.target.value)}></input>
-                        <button type="button"  autoFocus className='button-submit' onClick={handleSubmit}>ENTRAR</button>
+                        <button type="submit"  autoFocus className='button-submit'>ENTRAR</button>
                     </form>
                     <Link to='/forget' className='Link forget'>Esqueci minha senha</Link>
                     <div className='login'>
